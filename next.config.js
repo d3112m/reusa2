a741e9714888299270e1ce8c47658379455b067f
David Aaron Medeiro <davidrnmdr@gmail.com>
2025-07-06 19:32:19 -0300

    teste build 4

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const nextConfig = {
-  // Configuração para garantir que o ficheiro da base de dados SQLite seja incluído no build da Vercel.
-  // Isto é crucial para que o Prisma encontre a base de dados no ambiente de produção.
   experimental: {
     outputFileTracingIncludes: {
-      '/items': ['./prisma/dev.db'],
-      '/items/[id]': ['./prisma/dev.db'],
+      '/items': ['./prisma/dev.db', './prisma/schema.prisma'],
+      '/items/[id]': ['./prisma/dev.db', './prisma/schema.prisma'],
     },
   },
 };
