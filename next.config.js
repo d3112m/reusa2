7abd9f8b8184a402e2aea8e20553df38ae59a919
David Aaron Medeiro <davidrnmdr@gmail.com>
2025-07-06 19:54:15 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/**/*': ['./prisma/dev.db', './prisma/schema.prisma'],
+    },
+  },
 };
 
 module.exports = nextConfig;
