7eb1f66edec0bf92e0d3d6e2ded91d1d3823deaa
David Aaron Medeiro <davidrnmdr@gmail.com>
2025-07-06 21:13:04 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 const nextConfig = {
-  // Garante que o ficheiro da base de dados seja incluído no build
-  // para que possa ser copiado para /tmp em produção.
-  experimental: {
-    outputFileTracingIncludes: {
-      '/**': ['./prisma/dev.db'],
-    },
-  },
 };
 
 module.exports = nextConfig;
